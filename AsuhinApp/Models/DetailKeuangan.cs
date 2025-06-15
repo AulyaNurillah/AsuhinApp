777c32b98ff4fc144425a456025b4aa821053339
Aulya Nurillah <aulyarhma632@gmail.com>
2025-06-15 22:37:32 +0700

    Update model DetailKeuangan

--- AsuhinApp/Models/DetailKeuangan.cs
+++ AsuhinApp/Models/DetailKeuangan.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AsuhinApp.Models
+﻿namespace PantiApp3.Models
 {
-    internal class DetailKeuangan
+    public class DetailKeuangan
     {
+        public int IdDetail { get; set; }
+        public string TipeTransaksi { get; set; }
+        public decimal Saldo { get; set; }
+
+        public int? IdPemasukan { get; set; }
+        public int? IdPengeluaran { get; set; }
+        public int? IdDonasi { get; set; }
     }
 }
